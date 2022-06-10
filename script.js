dc60b2a23c65600c8415c4fca25b3e4fb1366fa8
LucasPriestley <lucas_priestley@outlook.com>
2022-06-10 12:35:37 +0100

    First Upload

---
+++ script.js
@@ -0,0 +1,72 @@
+//-------------weekOneSubmission---------------//
+
+//----Activity 1 -----//
+// create a variable called num. if num is divisible by 3 log fizz to the console, if its divisble by 5 log buzz to the console, if it's divisle by both 3 and 5 log fizzbuzz to the console. Otherwise log num to the console.
+
+// let num = 26 
+// if(num % 3 ==0 && num % 5 !=0){
+//     console.log("fizz")
+// }
+// else if(num % 5 ==0 && num %3 !=0){
+//     console.log("buzz")
+// }
+// else if (num % 3 ==0 || num % 5 ==0){
+//     console.log("fizz buzz")
+// }
+// else {
+//     console.log(num)
+// }
+
+//----Activity 2 ----//
+
+// function randomNum(){
+//     return Math.floor(Math.random() * 30) + 1; //---create a function that finds random num between 1-30
+// }
+// for (let i = 1; i < 7; i++){  //--loops 6 times
+//     let num = randomNum() //-- assigns random  num to variable
+//     console.log(num); 
+//     if (num % 7 ===0){ //--checks if random number is divisible by 7
+//         console.log("This is divisible by 7")
+
+//     }
+//     else{
+//         console.log("Not divisible by 7")
+//     }
+// }
+
+//----activity 3 ----//
+
+// let num = []
+// for(let x = 0; x < 12; x++)
+//     if (x % 2 == 0){
+//         console.log(Math.floor(Math.random() * 50))
+//         num.push(x)
+// }
+
+//---- Activity 4 ----//
+
+// for (let i = 0; i < 5; i++) {
+//     console.count();
+//   }
+
+
+// for (i = 9; i > -1; i--) {
+//     console.log(i)
+// }
+
+//---- Activity 5 ----//
+
+// let pet = {
+//     name: "Jon",
+//     typeOfPet: "ferrett",
+//     age: "15",
+//     colour: "black",
+//     eatAndDrink(){
+//         console.log(`${this.name}, is eating/drinking`)
+//     }
+// }
+
+// pet.eatAndDrink()
+
+//---- Actvity 6 ----//
+
